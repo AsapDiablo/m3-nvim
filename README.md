b76eb6715a94e1719481064448c16d2fd24e14ac
AsapDiablo <hyp547450291@qq.com>
2024-04-03 08:45:49 +0800

    feat: init

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # m3-nvim
+WIP
---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,18 @@
+
+return  {
+    "neovim/nvim-lspconfig",
+    config = function()
+   	require("lspconfig").gopls.setup {
+    settings = {
+      gopls = {
+        ui = {
+          completion = {
+            usePlaceholders = true,
+          },
+        },
+      },
+    },
+  }
+    end,
+  }
+
