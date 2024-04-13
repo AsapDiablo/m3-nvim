4d11f0f020465ab7ac1e35728144ac545acfd89b
huyipeng.momula <huyipeng.momula@bytedance.com>
2024-04-13 23:54:10 +0800

    go debug

---
+++ lua/plugins/debugging.lua
@@ -0,0 +1,33 @@
+return {
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "rcarriga/nvim-dap-ui",
+    "nvim-neotest/nvim-nio",
+    "leoluz/nvim-dap-go",
+  },
+  config = function()
+    local dap, dapgo, dapui = require("dap"), require("dap-go"), require("dapui")
+
+    dapgo.setup()
+    dapui.setup()
+    --require("dap-go").setup()
+    --require("dapui").setup()
+
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+
+    vim.keymap.set("n", "<Leader>bt", dap.toggle_breakpoint, {})
+    vim.keymap.set("n", "<Leader>bc", dap.continue, {})
+    vim.keymap.set("n", "<Leader>:r", dapgo.debug_test, {})
+  end,
+}
