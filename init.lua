eae68d18d5714feba80faf1876825df4f1e1e081
huyipeng.momula <huyipeng.momula@bytedance.com>
2024-04-13 21:50:32 +0800

    relearn

--- init.lua
+++ init.lua
@@ -1,6 +1,16 @@
--- 自定义配置
+-- typecraft learning
+-- lua vim command
 vim.o.number = true
 vim.o.relativenumber = true
+
+vim.opt.termguicolors = true
+
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+vim.g.mapleader = " "
 
 --lazy.nvim配置
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -14,9 +24,46 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     lazypath,
   })
 end
-
 vim.opt.rtp:prepend(lazypath)
 
-vim.opt.termguicolors = true
+local plugins = {
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  {
+    'nvim-treesitter/nvim-treesitter', build = ":TSUpdate"
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
+  },
+}
 
-require("lazy").setup({{import = "plugins"}})
+local opts = {}
+
+require("lazy").setup(plugins, opts)
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>t', ':Neotree filesystem reveal left<CR>')
+
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin"
+
+local config = require('nvim-treesitter.configs')
+config.setup({
+  ensure_installed = {"lua", "go"},
+  highlight = { enable = true },
+  indent = { enable = true },
+})
+
+local telescope = require("telescope")
+local telescopeConfig = require("telescope.builtin")
