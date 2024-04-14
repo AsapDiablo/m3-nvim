fcd2062b6d99b6f74a2fe99013a62cbf3fd5eec0
huyipeng.momula <huyipeng.momula@bytedance.com>
2024-04-14 08:09:46 +0800

    normal

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -8,7 +8,6 @@ local opts = {
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.gopls,
         null_ls.builtins.formatting.gofumpt,
         null_ls.builtins.formatting.gofmt,
         null_ls.builtins.formatting.goimports_reviser,
